db99089da496c1c2fe65bec92113354bb32d9489
Amaliexiao <amnie22@student.sdu.dk>
2024-02-29 15:01:14 +0100

    Asteroid implemented to spawn randomly. Collision module created

--- Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/AsteroidControlSystem.java
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/AsteroidControlSystem.java
@@ -8,32 +8,43 @@ import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 public class AsteroidControlSystem implements IEntityProcessingService {
     @Override
     public void process(GameData gameData, World world) {
+        if (1000 * Math.random() > 995) {
+            world.addEntity(spawnAsteroid(gameData));
+        }
+
         for (Entity asteroid : world.getEntities(Asteroid.class)) {
 
-            double changeX = Math.cos(Math.toRadians(asteroid.getRotation())) + 1 * Math.random();
-            double changeY = Math.sin(Math.toRadians(asteroid.getRotation())) + 1 * Math.random();
+            double changeX = Math.cos(Math.toRadians(asteroid.getRotation()));
+            double changeY = Math.sin(Math.toRadians(asteroid.getRotation()));
             asteroid.setX(asteroid.getX() + changeX);
             asteroid.setY(asteroid.getY() + changeY);
-            asteroid.setRotation(asteroid.getRotation() + 1 * Math.random());
 
-
-            if (asteroid.getX() < -2) {
-                asteroid.setX(gameData.getDisplayWidth() + 1);
+            if (asteroid.getX() < 0) {
+                world.removeEntity(asteroid);
             }
 
-            if (asteroid.getX() > gameData.getDisplayWidth() + 2) {
-                asteroid.setX(-1);
+            if (asteroid.getX() > gameData.getDisplayWidth()) {
+                world.removeEntity(asteroid);
             }
 
-            if (asteroid.getY() < -2) {
-                asteroid.setY(gameData.getDisplayHeight() + 1);
+            if (asteroid.getY() < 0) {
+                world.removeEntity(asteroid);
             }
 
-            if (asteroid.getY() > gameData.getDisplayHeight() + 2) {
-                asteroid.setY(-1);
+            if (asteroid.getY() > gameData.getDisplayHeight()) {
+                world.removeEntity(asteroid);
             }
 
         }
+    }
+    public Entity spawnAsteroid(GameData gameData) {
+
+        Entity asteroid = new Asteroid();
+        asteroid.setX(gameData.getDisplayWidth());
+        asteroid.setY(gameData.getDisplayHeight()*Math.random());
+        asteroid.setRotation(180);
+        asteroid.setPolygonCoordinates(-10,6,-4,10,6,10,10,5,8,-8,-2,-10,-8,-4);
+        return asteroid;
     }
 
 }
--- Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/AsteroidPlugin.java
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/AsteroidPlugin.java
@@ -5,21 +5,11 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 public class AsteroidPlugin implements IGamePluginService {
-
     public AsteroidPlugin() {
     }
 
     @Override
     public void start(GameData gameData, World world) {
-    }
-
-    private Entity spawnAsteroid(GameData gameData) {
-
-        Entity enemy = new Asteroid();
-        enemy.setPolygonCoordinates(-5,-5,10,0,-5,5);
-        enemy.setX(gameData.getDisplayWidth()+1);
-        enemy.setY(Math.random()*gameData.getDisplayHeight());
-        return enemy;
     }
 
     @Override
