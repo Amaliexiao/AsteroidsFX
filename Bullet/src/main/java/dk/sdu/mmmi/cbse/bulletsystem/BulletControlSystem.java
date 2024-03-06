ae91ccc07d92be8418c43e8a5a8f7c35d8e86bda
Amaliexiao <amnie22@student.sdu.dk>
2024-03-06 20:16:52 +0100

    Clean up

--- Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
+++ Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
@@ -4,7 +4,6 @@ import dk.sdu.mmmi.cbse.common.bullet.Bullet;
 import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
-import dk.sdu.mmmi.cbse.common.data.GameKeys;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 
