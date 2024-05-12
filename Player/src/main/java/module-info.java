81a2305e5c3b1a1706183a7d02f644eed1500db7
Amaliexiao <amnie22@student.sdu.dk>
2024-05-12 21:20:17 +0200

    Refactoring

--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -1,10 +1,7 @@
-
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 module Player {
-    exports dk.sdu.mmmi.cbse.playersystem;
     requires Common;
     requires CommonBullet;   
     uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
